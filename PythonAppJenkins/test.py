2099e88743f5e8f2879e54af0c7ba1a135d4233d
Sergio Fontes <sergio@computencial.com>
2021-07-28 21:18:54 +0100

    dsddd

--- PythonAppJenkins/test.py
+++ PythonAppJenkins/test.py
@@ -6,13 +6,13 @@ class TestCalculadora(unittest.TestCase):
         resultado = calculadora.add(2,2)
         self.assertEqual(resultado,4)
     def teste_adiciona_decimais(self):
-        resultado = calculadora.add(5.3,2)
+        resultado = calculadora.add('5.3',2)
         self.assertEqual(resultado,7.3)
     def teste_adiciona_string_inteiro(self):
         resultado = calculadora.add('sergio',2)
         self.assertEqual(resultado,'sergio2')
     def teste_adiciona_string_decimal(self):
-        resultado = calculadora.add('sergio',2.2)
+        resultado = calculadora.add('sergio','2.2')
         self.assertEqual(resultado,'sergio2.2')
     def teste_adiciona_string(self):
         resultado = calculadora.add('sergio',' fontes')
